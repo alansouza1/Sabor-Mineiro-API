65b78f78cd48842704f832747f7b609beaa7da19
alansouza1 <alansouza4001@gmail.com>
2026-04-11 14:52:50 -0300

    chore: initialize spring boot project with maven configuration

---
+++ src/main/java/com/sabormineiro/api/SaborMineiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.sabormineiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborMineiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborMineiroApiApplication.class, args);
+	}
+
+}
